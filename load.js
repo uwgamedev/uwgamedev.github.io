20de305e9442a5763c044d667c06b5f77be669b5
frankhan95 <frankhan95@gmail.com>
2016-10-05 18:41:25 -0700

    club projects

--- load.js
+++ load.js
@@ -35,16 +35,21 @@
 		var projects = this.responseXML.querySelectorAll("project");
 		for(var i = 0; i < projects.length; i++) {
 			var div = document.createElement("div");
+			var link = document.createElement("a");
+			link.href = projects[i].querySelector("url").textContent;
 			var thumbnail = document.createElement("img");
 			thumbnail.src = projects[i].querySelector("image").textContent;
 			thumbnail.alt = projects[i].querySelector("title").textContent;
 			thumbnail.style.height = '200px';
 			thumbnail.style.width = '200px';
+			link.innerHTML = thumbnail;
 			var title = document.createElement("p");
 			title.innerHTML = projects[i].querySelector("title").textContent;
+			title.style.margin = "0";
 			var caption = document.createElement("p");
 			caption.innerHTML = projects[i].querySelector("author").textContent + " " + projects[i].querySelector("date").textContent;
-			div.appendChild(thumbnail);
+			caption.style.margin = "0";
+			div.appendChild(link);
 			div.appendChild(title);
 			div.appendChild(caption);
 			div.className = "games";
